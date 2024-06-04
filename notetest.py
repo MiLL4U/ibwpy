4dfdcaefebaa0d62b8484a497254d10e6bae169e
MiLL4U <aphiloboe@gmail.com>
2024-06-04 16:59:16 +0900

    Revert "delete temporal files for testing"
    This reverts commit 742a60c58670ab539fb4e4b7b19032580099cf7c.

---
+++ notetest.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+import ibwpy as ip
+
+test_arr = np.array([
+    [0.0, 0.1, 0.2],
+    [0.1, 0.2, 0.3],
+    [0.2, 0.3, 0.4]
+], dtype=np.float32)
+
+# eng = ip.from_nparray(test_arr, "eng")
+# eng.set_note("The quick brown fox jumps over the lazy dog.")
+
+jp = ip.from_nparray(test_arr, "jp")
+jp.set_note("生物構造化学分野")
+
+# print(eng)
+# eng.save()
+
+print(jp.array)
+jp.save()
+
+jp_igor = ip.load("./jp_igor.ibw")
+print(jp_igor.array)
+print(jp_igor.note)
+print()
+jp_igor.save("./jp_igor_resave.ibw")
